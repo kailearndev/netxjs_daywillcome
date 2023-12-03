002082c581eda5b800d5ab0ac57d854c036c49f9
kai <kai@brickmate.vn>
2023-12-03 22:15:55 +0700

    fix done avater

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b3f48f05aff64ea"; }
+/******/ 		__webpack_require__.h = function() { return "b14e9d11ea1ffaa4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
